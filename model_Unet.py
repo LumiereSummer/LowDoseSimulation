5617e625e80f72abc161d53facecfeab2b235e18
Lumi <67916116+LumiereSummer@users.noreply.github.com>
2023-03-09 17:13:34 +0100

    Update model_Unet.py

--- model_Unet.py
+++ model_Unet.py
@@ -1,3 +1,6 @@
+#u-net model, with different filter numbers
+#the one called "generator33" is used for generating the results in the paper
+
 import tensorflow as tf
 from tensorflow.keras.models import Sequential, Model
 from tensorflow.keras.layers import Layer, Dense, Flatten, Dropout, Reshape, Input, Concatenate, add, UpSampling2D
@@ -428,30 +431,3 @@ def generator33(input_shape):
 
 
 
-
-import matplotlib
-import matplotlib.pyplot as plt
-from datetime import datetime
-
-class timecallback(tf.keras.callbacks.Callback):
-    def __init__(self):
-        self.times = []
-        self.epochs = []
-        # use this value as reference to calculate cummulative time taken
-        self.timetaken = tf.timestamp()
-    def on_epoch_end(self,epoch,logs = {}):
-        self.times.append(tf.timestamp() - self.timetaken)
-        self.epochs.append(epoch)
-    def on_train_end(self,logs = {}):
-        plt.figure()
-        plt.xlabel('Epoch')
-        plt.ylabel('Total time taken until an epoch in seconds')
-        plt.plot(self.epochs, self.times, 'ro')
-        for i in range(len(self.epochs)):
-          j = self.times[i].numpy()
-          if i == 0:
-            plt.text(i, j, str(round(j, 3)))
-          else:
-            j_prev = self.times[i-1].numpy()
-            plt.text(i, j, str(round(j-j_prev, 3)))
-        plt.savefig(datetime.now().strftime('%Y%m%d%H%M%S') + '.png')
