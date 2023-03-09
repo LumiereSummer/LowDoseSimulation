9b5da2a837edd4a6dc5a15fb1ec58d4e8ece3f94
Lumi <67916116+LumiereSummer@users.noreply.github.com>
2023-03-09 13:50:10 +0100

    Update application.py

--- application.py
+++ application.py
@@ -43,50 +43,72 @@ from models import *
 
 
 
+***
+image preparation
+***
 
+pathref='/path/to/reference/dicomIMAGES/'
 
-dspath32='/media/lxia/Expansion/DATA/Patient32/IMAGES/'
+dspath32='/path/to/patientX/original/dicomIMAGES/'
+pathsrc32='/path/to/store/patientX/selected/pngIMAGES/'
+pathmatch32='/path/to/store/patientX/HistogramMatched/selected/pngIMAGES/'
 
-pathref='/media/lxia/Expansion/LowDose/lowdoseTest/IMAGES/'
-pathsrc32='/media/lxia/Expansion/LDSimulation/DL/PC195/DL_ICASSP/experiment/P32/source/'
-pathmatch32='/media/lxia/Expansion/LDSimulation/DL/PC195/DL_ICASSP/experiment/P32/match/'
+#take 10 example images of patient X
+imnms32=random.sample(os.listdir(dspath32),10)
+#imnms32=rmpng(os.listdir(pathsrc32))
 
-
-imnms75=random.sample(os.listdir(dspath75),10)
-#imnms75=rmpng(os.listdir(pathsrc75))
-
-
+#get the names of all reference images
+imnms120=rmpng(os.listdir('/path/to/reference/pngIMAGES/120kvp/'))
+#get dicom image,save png image
 ims32=[]
 for nm in imnms32:
     imm=readdicom(nm,dspath32,'midastinum')
     plt.imsave(os.path.join(pathsrc32,nm)+'.png',imm,cmap='gray')
     im=readdicom(nm,dspath32,None)
     ims32.append(im)
-    
 
+#Images Standardization
+#Histogram Matching to generate matched dicom imagr as input
 immatched32=imshistmatch(imnms32, dspath32, imnms120, pathref,pathmatch32)
 
 
-outputsim_rm32="/media/lxia/Expansion/LDSimulation/DL/PC195/DL_ICASSP/experiment/P32/output_match/"
-outputdif_rm32="/media/lxia/Expansion/LDSimulation/DL/PC195/DL_ICASSP/experiment/P32/diff_match/"
+***
+Model Output: Desired Low dose image Simulation
+***
+
+outputsim_rm32='/path/to/store/patientX/simulated/lowDose/IMAGES/'
+outputdif_rm32='/path/to/store/patientX/modelOutput/of/selected/IMAGES/'
+
+#model output
 imgouputs_rm32=model213output(checkpoint,np.array(immatched32))
 
-
+#find the best scaling factor for achieving the desired dose level, by histogram correlation
 findscale(imnms80,pathref,imnms32,ims32,imgouputs_rm32,"histogram_correlation_k_p32.csv")
 
-
+#save simulated low dose image
 imgoutputs_rm32=[]
+#scaling factor for customizing the dose
+k=2.5
 for i in range(len(imgouputs_rm32)):
     #print(i)
     nm=imnms32[i]
-    plt.imsave(outputdif_rm32+nm+'_2.5.png',2.5*pthres0(imgouputs_rm32[i],250),cmap='gray')
-    img=readdicomm(nm,ims32[i]-2.5*pthres0(imgouputs_rm32[i],200),dspath32,'midastinum')+10
+    plt.imsave(outputdif_rm32+nm+'_'+str(k)+'.png',k*pthres0(imgouputs_rm32[i],250),cmap='gray')
+    img=readdicomm(nm,ims32[i]-k*pthres0(imgouputs_rm32[i],200),dspath32,'midastinum')+10
     imgoutputs_rm32.append(img)
-    plt.imsave(outputsim_rm32+nm+'_2.5.png',img,cmap='gray')
+    plt.imsave(outputsim_rm32+nm+'_'+str(k)+'.png',img,cmap='gray')
+
+    
 
 ***
-histogram matching
+Histogram Matching Visualization of one image: Cumulative Histogram
 ***
+
+nm_ref=os.path.join(pathref,imnms120[0])
+im_ref=readdicom(nm_ref,pathref,None)
+
+source=ims32[0]
+matched=immatched32[0]
+template=im_ref
 x1, y1 = ecdf(source.ravel())
 x2, y2 = ecdf(template.ravel())
 x3, y3 = ecdf(matched.ravel())
